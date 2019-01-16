edd5a2ced6fd50b2c254ac2b663bb1f01c4da8c0
vlad <kt351b@gmail.com>
2019-01-16 13:49:42 +0200

    added new branch iss1

--- queue_notificator.py
+++ queue_notificator.py
@@ -4,88 +4,99 @@
 import os
 import smtplib
 import datetime
+import logging
+from logging.handlers import WatchedFileHandler
+import sys
 
-# Set queue name
+# ----- logger settings:
+logger = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# ---- syslog file settings:
+fh = logging.handlers.WatchedFileHandler('/var/log/asc.log')
+# For log-file output use logging.INFO to write to log
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+
+# queue name
 queue = 'tech-support'
-# Flag to send e-mail
-send_flag = 0
+# from what waittime send e-mail
+max_waittime = 1
 
-# Get string - "queue tech-support has N calls"
-get_calls_quant = """asterisk -x 'queue show %s' | grep %s | awk '{print $1, $2, $3, $4}'""" % (queue, queue)
-#calls_quant = os.popen(get_calls_quant).read()[0:1]
-calls_quant = os.popen(get_calls_quant).read()
-# For testing at local machine:
-#calls_quant = os.popen("cat /path_to_file/test_queue | grep queue-921 | awk '{print $1, $2, $3, $4}'").read()
-
-# Get waiting in queue callers ChannelID
-callers_list = {}
-get_callers = """asterisk -x 'queue show %s' | grep wait | awk '{print $2}'""" % (queue)
-SIP_list = os.popen(get_callers).read()
-# For testing at local machine:
-#SIP_list = os.popen("cat /path_to_file/test_queue | grep wait | awk '{print $2}'").read()
-SIP_list = SIP_list.splitlines()
-k = 0
-for i in SIP_list:
-    SIP_list[k] = i[0:-2]
-    k += 1
-
-# Get callers telephone numbers, using CallerID from "asterisk show queue queue_name":
-for channel in SIP_list:
-    get_callerid = """asterisk -x 'core show channels verbose' | grep %s | grep %s | awk '{print $8, $9}'""" % (queue, channel)
-    # For testing at local machine:
-    #get_callerid = """cat /path_to_file/test_channel | grep %s | grep %s | awk '{print $8, $9}'""" % (queue, channel)
-    callers = os.popen(get_callerid).read().split('\n')
-    del callers[-1]
-    for i in callers:
+# get waittime in queue
+get_waittime = """ asterisk -x 'queue show %s' | grep wait | awk '{print $4}' | awk -F ':' '{print $1}'""" %(queue)
+waittime = os.popen(get_waittime).read().rsplit('\n')
+waittime = filter(None, waittime)
+waittime = [int(i) for i in waittime]
+if max(waittime or [0]) >= max_waittime:
+    logging.info("waittime is more then 1 minute: {}".format(waittime) )
+    # get quantity of calls in queue
+    calls_num = """asterisk -x 'queue show %s' | grep %s | awk '{print $3}'""" % (queue, queue)
+    calls = os.popen(calls_num).read()
+    #logging.info("calls_num: {}".format(calls_num) )
+    #logging.info("calls: {}".format(calls) )
+    dict_chan = {} 
+    #get channels list with waittime 
+    channels = {}
+    get_channels = """ asterisk -x 'queue show %s' | grep wait | awk '{print $2, $4}'""" %(queue)
+    #logging.info("get_channels: {}".format(get_channels) )
+    channels = os.popen(get_channels).read().rsplit('\n')
+    channels = filter(None, channels)
+    logging.info("channels: {}".format(channels) )
+    #compose dict_chan[channels]=waittime
+    for i in channels:
         i = i.split()
-        value = callers_list.get(i[0])
-        if value:
-            value.append(i[1])
-            callers_list[i[0]] = value
-        else:
-            callers_list[i[0]] = i[1].split()
-
-
-# Delete same waittime values in callers_list dictionary
-# and set send_flag if waittime is more then 1 minute
-def del_same(dic, send_flag):
-    for i in dic:
-        lst = []
-        for k in dic[i]:
-            if k not in lst:
-                lst.append(k)
-                if int(k[-5:-3]) >=1:
-                    send_flag = 1
-        dic[i] = lst
-    return dic, send_flag
-
-
-callers_list, send_flag = del_same(callers_list, send_flag)
-
-if send_flag >= 1:
-    # Set sender and receivers for e-mail:
-    sender = 'info@domain.net'
-    receivers = ['admin@domain.net', 'voip@domain.net']
-    # Compose callers numbers and waititme for message:
-    callers_message = ''
-    for i in callers_list:
-        a = ''
-        for k in callers_list[i]:
-            a = '%s \t\t %s \n' % (i, k[3:])
-            callers_message = callers_message + a
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d (%A) %H:%M")
-    # This is not an indent error, message should be written with such indents
-    message = """From: info<info@domain.net>
-To: admin <admin@domain.net>
-Subject: calls in queue
-At %s there is/are calls with more then 1 minute waiting in queue.
-%s 
-caller is waiting for (min:sec):
-%s
-""" % (current_time, calls_quant.rstrip('\n'), callers_message)
+        dict_chan[''.join(i[0])]=''.join(i[1])
+    logging.info("channels: {}".format(dict_chan) )
+    # get number of callers, using channels:
+    lst = [i for i in dict_chan]
+    str_grep = ""
     try:
-        smtpObj = smtplib.SMTP('smtp.domain.net')
-        smtpObj.sendmail(sender, receivers, message)
-        print("Successfully sent email")
-    except smtplib.SMTPException:
-        print("Error: unable to send email")
+        for i in range(1, len(lst)-1):
+            str_grep = str_grep+"%s\\|" % lst[i]
+        st = "| grep \"%s\\|" % lst[0]
+        str_grep = " asterisk -x 'core show channels concise'" + st + str_grep + "%s\"" % lst[len(lst)-1]
+        #asterisk -x 'core show channels concise'| grep "SIP/656148136-000945aa\|SIP/921-000945a6"
+        logging.info("str_grep: {}".format(str_grep) )
+        # get callerid:
+        chan_lst = os.popen(str_grep).read().rsplit('\n')
+        chan_lst = filter(None, chan_lst)
+        for i in chan_lst:
+            i = i.split('!')
+            tmp = dict_chan.get(i[0]).split(',')
+            del tmp[1]
+            tmp.append(i[7])
+            dict_chan[i[0]] = tmp
+        #compose text for message:
+        callers_message = ''
+        for i in dict_chan:
+            a = ''
+            a = '%-10s %15s \n' %(dict_chan.get(i)[1], dict_chan.get(i)[0])
+            callers_message = callers_message + a
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d (%A) %H:%M")
+        sender = 'fax-server@some.net.ua'
+        # receivers templates:
+        #receivers = ['admin@some.net','someone@some.net']
+        receivers = ['admin@some.net']
+        
+        message = """From: fax-server<fax-server@some.net>
+To: admin <admin@some.net>
+Subject: %s calls in queue
+At %s there is/are calls with more then 1 minute waiting in queue.
+queue %s has %s calls
+abonent is waiting for (min:sec):
+%s
+""" % (calls.rstrip('\n') , current_time, queue, calls.rstrip('\n'), callers_message)
+        logging.info("message: {}".format(message) )
+        try:
+            smtpObj = smtplib.SMTP('smtp.some.net.ua')
+            smtpObj.sendmail(sender, receivers, message)
+            logging.info("Succesfully send mail")
+            logging.info("END OF SCRIPT --------------------")
+        except smtplib.SMTPException:
+            logging.info("Error: unable to send email")
+            logging.info("END OF SCRIPT --------------------")
+    except IndexError:
+        sys.exit()
